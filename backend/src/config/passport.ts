dfdd7f324d0e9e13fb26a05ce9f35ff2b85b876b
raxit.bambharoliya <raxit.bambharoliya@artoon.in>
2024-07-11 16:43:06 +0530

    passport up

---
+++ backend/src/config/passport.ts
@@ -0,0 +1,32 @@
+
+
+
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+import { Profile, VerifyCallback } from "passport-google-oauth20";
+
+passport.use(
+  new GoogleStrategy(
+    {
+		clientID: process.env.CLIENT_ID ||"" ,
+					clientSecret: process.env.CLIENT_SECRET||"",
+					callbackURL: "/auth/google/callback",
+					scope: ["profile", "email"],
+    },
+	function (accessToken: string, refreshToken: string, profile: Profile, done: (error: any, user?: any) => void) {
+		if (profile) {
+		  done(null, profile);
+		} else {
+		  done(null, false);
+		}
+	  }
+  )
+);
+
+passport.serializeUser((user: Express.User, done: (err: any, id?: unknown) => void) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user:any, done: (err: any, id?: unknown) => void) => {
+  done(null, user);
+});
