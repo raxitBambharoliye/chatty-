ca62e91fc7498ec0f0c1aae487f0c30d8dc18fea
raxit.bambharoliya <raxit.bambharoliya@artoon.in>
2024-07-11 16:29:13 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,13 @@ import {Server} from 'socket.io'
 import { socketConnection } from './connection/socket.connection';
 import logger from './utility/logger';
 import router from './router';
-import  cors from 'cors'
+import cors from 'cors'
+import cookieSession from 'cookie-session';
+import passport from 'passport'
+import './config/passport'
+import { googleRouter } from './router/google.router';
+import Cookies from 'cookie-parser'
+
 dotenv.config()
 const app = express();
 const httpServer = createServer(app)
@@ -22,16 +28,39 @@ const io = new Server(httpServer, {
 
 app.use(cors())
 app.use(express.json());
-
+app.use(Cookies());
+app.use(
+    cookieSession({
+        name: "google",
+        keys: ["chattyPie"],
+        maxAge:24*60*60
+    })
+)
+// register regenerate & save after the cookieSession middleware initialization
+app.use(function(request, response, next) {
+    if (request.session && !request.session.regenerate) {
+        request.session.regenerate = (cb:any) => {
+            cb()
+        }
+    }
+    if (request.session && !request.session.save) {
+        request.session.save = (cb:any) => {
+            cb()
+        }
+    }
+    next()
+})
+app.use(passport.initialize())
+app.use(passport.session())     
 app.get("/", (req: any, res: any) => {
-    console.log("test check api calling ")
-    console.log("check token ", req.headers['x-auth-token']);
     res.status(200).send({
         message:"server api call test successfully"
     })
 })
 
-app.use("/",router)
+app.use("/", router)
+app.use('/auth',googleRouter)
+
 httpServer.listen(process.env.SERVER_PORT, async() => {
     await mongoDbConnection();
     await socketConnection();
