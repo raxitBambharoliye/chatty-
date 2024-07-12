6f9fcc91312585f46c226c00bd14cef434de93a3
raxit.bambharoliya <raxit.bambharoliya@artoon.in>
2024-07-12 11:28:03 +0530

    passport

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -5,13 +5,16 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import passport from "passport";
 import { Profile, VerifyCallback } from "passport-google-oauth20";
 
+
+const clientSecret = process.env.CLIENT_SECRET || "";
+const clientID = process.env.CLIENT_ID || "";
 passport.use(
   new GoogleStrategy(
     {
-		clientID: process.env.CLIENT_ID ||"" ,
-					clientSecret: process.env.CLIENT_SECRET||"",
-					callbackURL: "/auth/google/callback",
-					scope: ["profile", "email"],
+		clientID ,
+		clientSecret,
+		callbackURL: "/auth/google/callback",
+		scope: ["profile", "email"],
     },
 	function (accessToken: string, refreshToken: string, profile: Profile, done: (error: any, user?: any) => void) {
 		if (profile) {
